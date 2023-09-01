2fdfe428cc737b8df8f95ecd65ad37c322ec3e44
NicoNethy <5761@holbertonstudents.com>
2023-09-01 05:51:56 -0700

    debugging

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,16 +7,16 @@
  **/
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node;
+    binary_tree_t *node;
 
-	if (parent == NULL)
+    if (parent == NULL)
     {
-	return (NULL);
+    return (NULL);
     }
 
-	node = malloc(sizeof(binary_tree_t));
+    node = malloc(sizeof(binary_tree_t));
 
-	if (node == NULL)
+    if (node == NULL)
     {
 	return (NULL);
     }
