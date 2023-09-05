30c354628a56200b09fa060d90ae76006f698254
NicoNethy <5761@holbertonstudents.com>
2023-09-05 14:25:49 -0700

    little eigth fix

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -8,12 +8,14 @@
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if(tree == NULL || func == NULL)
-        return;
+	if (tree == NULL || func == NULL)
+	return;
 
-    binary_tree_postorder(tree->left, func);
-    binary_tree_postorder(tree->right, func);
-    
-    func(tree->n);
-    printf("%d ", tree->n);
+	if (tree->left != NULL)
+	binary_tree_postorder(tree->left, func);
+
+	if (tree->right != NULL)
+	binary_tree_postorder(tree->right, func);
+
+	func(tree->n);
 }
