cf1f8daae9a3f0cdcafa940d91d917a8d9de4d2b
cfranco87 <carlosfrancoart@gmail.com>
2023-09-05 09:11:37 -0700

    fixing betty #6

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,18 +1,19 @@
 #include "binary_trees.h"
 
 /**
- *binary_tree_preorder- a type of tree traversal that follows the Root-Left-Right policy 
- *@tree: tree structure 
- Return: check on tree structure
+ *binary_tree_preorder- tree traversal that follows the Root-Left-Right
+ *@tree: tree structure
+ *@func: calling function
+ *Return: check on tree structure
 */
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if(tree == NULL || func == NULL)
-        return;
-    
-    func(tree->n);
+	if (tree == NULL || func == NULL)
+		return;
 
-    binary_tree_preorder(tree->left, func);
-    binary_tree_preorder(tree->right, func);
+	func(tree->n);
+
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
