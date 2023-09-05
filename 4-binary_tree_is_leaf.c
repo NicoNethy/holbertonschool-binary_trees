175a890fd9ad3c6b62596383553f210f66cce528
cfranco87 <carlosfrancoart@gmail.com>
2023-09-05 08:53:04 -0700

    betty #3-4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -3,17 +3,17 @@
 /**
  *binary_tree_is_leaf - checking to see if binary tree is a leaf
  *@node: node to check leaf status
- Return: 1 if its leaf 
+ *Return: 1 if its leaf
 */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if(node == NULL)
-        return(0);
+	if (node == NULL)
+		return (0);
 
-    if(node->right == NULL && node->left == NULL)
-        return(1);
+	if (node->right == NULL && node->left == NULL)
+		return (1);
 
-return(0);
+return (0);
 
 }
