66dd495d15a933c9cb180515b698838ab08bbbd3
NicoNethy <5761@holbertonstudents.com>
2023-09-05 14:51:35 -0700

    14 fix please

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -3,20 +3,20 @@
 
 /**
  *binary_tree_balance - finding if the tree is balanced
- *@tree: tree structure 
+ *@tree: tree structure
  Return: balanced or not
 */
 
 int binary_tree_balance (const binary_tree_t *tree)
 {
-    if(tree == NULL)
-        return(0);
+	if(tree == NULL)
+		return(0);
 
-    int left_height = binary_tree_height(tree->left);
-    int right_height = binary_tree_height(tree->right);
-    
-    if (abs (left_height - right_height) <= 1 && binary_tree_height(tree->left) && binary_tree_height(tree->right))
-        return(1);
+	int left_height = binary_tree_height(tree->left);
+	int right_height = binary_tree_height(tree->right);
 
-    return(0);
+	if (abs (left_height - right_height) <= 1 && binary_tree_height(tree->left) && binary_tree_height(tree->right))
+		return(1);
+
+	return(0);
 }
