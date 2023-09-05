38679ec2ba14e7f331d208ad966e6823ae66bc54
NicoNethy <5761@holbertonstudents.com>
2023-09-05 14:38:02 -0700

    ten ten

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -2,15 +2,18 @@
 
 /**
  *binary_tree_depth- finding the depth of a binary tree
- *@tree: tree structure 
+ *@tree: tree structure
  Return: depth of tree structure
 */
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	if (tree == NULL || tree->parent == NULL)
+    {
 	    return (0);
-
+    }
 	else
+    {
     	return (binary_tree_depth(tree->parent) + 1);
+    }
 }
